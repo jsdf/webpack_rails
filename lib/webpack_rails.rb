b606778b369714a17e7db9b762d6297fb36f4aee
James Friend <james@jsdf.co>
2016-01-19 11:40:10 +1100

    fixed gem

--- lib/webpack_rails.rb
+++ lib/webpack_rails.rb
@@ -2,5 +2,6 @@ module WebpackRails
   require_relative './webpack_rails/require_directive_processor'
   require_relative './webpack_rails/task'
   require_relative './webpack_rails/processor'
+  require_relative './webpack_rails/helper'
   require_relative './webpack_rails/engine'
 end
